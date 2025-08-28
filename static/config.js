b13587f14e33c3b2a9b165e4d9097b4febd84d3b
sebhindspy <sebastianhinds@hotmail.com>
2025-08-28 15:26:52 +0100

    fix: premium res api call

--- static/config.js
+++ static/config.js
@@ -1,5 +1,7 @@
 // Configure this to your API Gateway base URL, e.g., "https://abc123.execute-api.us-east-1.amazonaws.com/prod"
 // In local dev, you can leave it empty string to target same-origin FastAPI server
-window.API_BASE = "";
+//window.API_BASE = "";
+
+window.API_BASE = "https://x6vlu0t4s9.execute-api.eu-north-1.amazonaws.com/prod";
 
 
